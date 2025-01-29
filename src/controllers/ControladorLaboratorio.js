b35b7c7b23bc6de457f600c5fda9f7dc31572863
Isaac4Lion <isaac1leon@gmail.com>
2025-01-28 20:26:22 -0500

    fix: order codes labs

--- src/controllers/ControladorLaboratorio.js
+++ src/controllers/ControladorLaboratorio.js
@@ -21,7 +21,7 @@ export const crearLaboratorio = async (req, res) => {
 
 export const listarLaboratorios = async (req, res) => {
   try {
-    const laboratorios = (await Laboratorio.find()).sort('codigo');
+    const laboratorios = (await Laboratorio.find()).sort({ codigo: 1 });
     res.json(laboratorios);
   } catch (error) {
     console.error('Error al listar laboratorios:', error);
